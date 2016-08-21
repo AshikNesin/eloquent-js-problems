bf785d71f1c617dfaee31e3a3f073a79ab590d38
Ashik Nesin <mail@ashiknesin.com>
2016-08-21 13:39:14 +0530

    Solved FizzBuzz

--- 02-fizzbuzz.js
+++ 02-fizzbuzz.js
@@ -1,7 +1,12 @@
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-    	if(i%3 !==0 && i % 5 )
-        var word = (i % 3 === 0) ? 'Fizz' : 'Buzz';
+    	var word='';
+    	if(i%3 ===0 && i % 5 ===0 ) {
+    		word = 'FizzBuzz';
+    	}
+    	else{
+         word = (i % 3 === 0) ? 'Fizz' : 'Buzz';
+    	}
         console.log(word);
     } else {
         console.log(i)
